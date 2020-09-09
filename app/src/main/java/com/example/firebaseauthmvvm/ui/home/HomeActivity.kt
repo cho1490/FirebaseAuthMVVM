97855245c3d2b7f7f4572c76652fd7bbf170178c
cho1490 <chosanghyun99@gmail.com>
2020-09-09 21:07:06 +0900

    Add FirebaseSource in data.firebase

--- app/src/main/java/com/example/firebaseauthmvvm/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/firebaseauthmvvm/ui/home/HomeActivity.kt
@@ -1,12 +1,12 @@
-package com.example.firebaseauthmvvm.ui.auth
+package com.example.firebaseauthmvvm.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firebaseauthmvvm.R
 
-class LoginActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_home)
     }
 }
